365b4828dac05c295643a055edc4d5271a72e04c
Deigo <diegodelgado365@gmail.com>
2023-06-19 11:56:57 -0300

    corrigiendo botón de menu hamburguesa en contacto

--- js/function.js
+++ js/function.js
@@ -1,8 +1,8 @@
 function enviarConsulta()
 {
     let enlace= document.getElementById("botonContacto");
-    let mensaje="https://wa.me/543515196190?text=Hola, mi nombre es *"+ document.getElementById("nombre").value+"*\n, email: "+document.getElementById("email").value+",\n teléfono *"+
-     document.getElementById("telefono").value+"* y vivo en "+document.getElementById("provincia").value+".\n Necesito información sobre *"+document.getElementById("consulta").value
+    let mensaje="https://wa.me/543515196190?text=Hola, mi nombre es "+ document.getElementById("nombre").value+", email: "+document.getElementById("email").value+", teléfono "+
+     document.getElementById("telefono").value+" y vivo en "+document.getElementById("provincia").value+".\n Necesito información sobre *"+document.getElementById("consulta").value
     enlace.href=mensaje+"*";
 
 return mensaje
