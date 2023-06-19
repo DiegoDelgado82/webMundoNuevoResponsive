adb0858713edaab59b3ce04590f2f4fe548a3fbd
Deigo <diegodelgado365@gmail.com>
2023-06-19 11:51:01 -0300

    codificando botón consulta

---
+++ js/function.js
@@ -0,0 +1,9 @@
+function enviarConsulta()
+{
+    let enlace= document.getElementById("botonContacto");
+    let mensaje="https://wa.me/543515196190?text=Hola, mi nombre es *"+ document.getElementById("nombre").value+"\n*, email: "+document.getElementById("email").value+",\n teléfono *"+
+     document.getElementById("telefono").value+"* y vivo en "+document.getElementById("provincia").value+".\n Necesito información sobre *"+document.getElementById("consulta").value
+    enlace.href=mensaje+"*";
+
+return mensaje
+}
