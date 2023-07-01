8fb35308f06887b3569a49109b9f1c437729d0ef
Diego Delgado <diegodelgado365@gmail.com>
2023-07-01 10:41:16 -0300

    acomodando textos

--- js/function.js
+++ js/function.js
@@ -1,7 +1,7 @@
 function enviarConsulta()
 {
     let enlace= document.getElementById("botonContacto");
-    let mensaje="https://wa.me/543515196190?text=Hola, mi nombre es "+ document.getElementById("nombre").value+", email: "+document.getElementById("email").value+", teléfono "+
+    let mensaje="https://wa.me/543513446478?text=Hola, mi nombre es "+ document.getElementById("nombre").value+", email: "+document.getElementById("email").value+", teléfono "+
      document.getElementById("telefono").value+" y vivo en "+document.getElementById("provincia").value+".\n Necesito información sobre *"+document.getElementById("consulta").value
     enlace.href=mensaje+"*";
 
